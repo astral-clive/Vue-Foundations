499d972098e87258d6b73fa82a770f098065a884
Paul <hello@paulhitchmough.com>
2020-03-13 13:58:19 +0100

    basic html page with materialize and vm

--- alpha/js/App.js
+++ alpha/js/App.js
@@ -1,14 +1,10 @@
-// Define a new component called button-counter
-Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+Vue.component('blog-post', {
+  props: ['title'],
+  template: '<h3>{{ title }}</h3>'
 })
 
 var vm = new Vue({
   el: '#App',
 
 })
+
