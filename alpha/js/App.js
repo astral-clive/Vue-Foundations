2f698d0ee958eec1a28af8b8b9cc3e4612252dbe
Paul <hello@paulhitchmough.com>
2020-03-13 13:25:52 +0100

    added alpha and external source for App

---
+++ alpha/js/App.js
@@ -0,0 +1,14 @@
+// Define a new component called button-counter
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+var vm = new Vue({
+  el: '#App',
+
+})
