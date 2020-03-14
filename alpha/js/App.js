44ee71524e37b410b43b3a446c22e736053d4fe3
Paul <hello@paulhitchmough.com>
2020-03-14 13:12:20 +0100

    add content to posts

--- alpha/js/App.js
+++ alpha/js/App.js
@@ -1,12 +1,12 @@
 var posts = [
-  { id: 1, title: 'Bakeries in Copenhagen' },
-  { id: 2, title: 'Apples in Santa Monica' },
-  { id: 3, title: 'Museums in Florence' },
+  { id: 1, title: 'Bakeries in Copenhagen', content: 'This is the content' },
+  { id: 2, title: 'Apples in Santa Monica', content: 'This is the content' },
+  { id: 3, title: 'Museums in Florence', content: 'This is the content' },
 ];
 
 Vue.component('blog-post', {
-  props: ['title'],
-  template: '<h3>{{ title }}</h3>'
+  props: ['title', 'content'],
+  template: '<div class="blog-post"><h3>{{ title }}</h3><div v-html="content">{{ content }}</div></div>'
 })
 
 var vm = new Vue({
