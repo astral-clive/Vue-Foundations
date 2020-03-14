ed5b78ac211b72ffa9a4c98b63d0b3dd2d41ea8f
Paul <hello@paulhitchmough.com>
2020-03-14 13:16:47 +0000

    added tab structure example

--- alpha/js/App.js
+++ alpha/js/App.js
@@ -21,3 +21,29 @@ var vm = new Vue({
   }
 })
 
+
+// example of tab structure
+
+Vue.component("tab-home", {
+  template: "<div>Home component</div>"
+});
+Vue.component("tab-posts", {
+  template: "<div>Posts component</div>"
+});
+Vue.component("tab-archive", {
+  template: "<div>Archive component</div>"
+});
+
+new Vue({
+  el: "#dynamic-component-demo",
+  data: {
+    currentTab: "Home",
+    tabs: ["Home", "Posts", "Archive"]
+  },
+  computed: {
+    currentTabComponent: function() {
+      return "tab-" + this.currentTab.toLowerCase();
+    }
+  }
+});
+
