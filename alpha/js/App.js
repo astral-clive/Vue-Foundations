b012607f2811e7effeca2d131fe0683ac43f2e4c
Paul <hello@paulhitchmough.com>
2020-03-14 12:50:09 +0100

    posts brought in from array

--- alpha/js/App.js
+++ alpha/js/App.js
@@ -1,3 +1,9 @@
+var posts = [
+  { id: 1, title: 'Bakeries in Copenhagen' },
+  { id: 2, title: 'Apples in Santa Monica' },
+  { id: 3, title: 'Museums in Florence' },
+];
+
 Vue.component('blog-post', {
   props: ['title'],
   template: '<h3>{{ title }}</h3>'
@@ -5,6 +11,8 @@ Vue.component('blog-post', {
 
 var vm = new Vue({
   el: '#App',
-
+  data: {
+    posts: posts
+  }
 })
 
