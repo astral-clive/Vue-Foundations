40d294d7e96f29cd557beacccff3ab529238f669
Paul <hello@paulhitchmough.com>
2020-03-14 13:20:01 +0100

    refactored to use multiline string variables

--- alpha/js/App.js
+++ alpha/js/App.js
@@ -6,7 +6,10 @@ var posts = [
 
 Vue.component('blog-post', {
   props: ['post'],
-  template: '<div class="blog-post"><h3>{{ post.title }}</h3><div v-html="post.content"></div></div>'
+  template: '<div class="blog-post">\
+  <h3>{{ post.title }}</h3>\
+  <div v-html="post.content"></div>\
+  </div>'
 })
 
 var vm = new Vue({
